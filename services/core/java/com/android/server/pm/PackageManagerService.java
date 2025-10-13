58f0b9f12c6e21643f80ef9d22a003b04f1d161a
Nate Myren <ntmyren@google.com>
2025-10-13 15:11:14 -0700

    [BACKPORT] Apps clearing self data shouldn't get permissions restored
    when an app calls clearApplicationUserData for itself, do not restore
    default or role granted permissions. However, maintain a privileged
    version that can.
    
    Bug: 339109116
    Test: atest ActivityManagerTest
    Flag: EXEMPT CVE_FIX
    Cherrypick-From: https://googleplex-android-review.googlesource.com/q/commit:86aab9fb0242fed4d773b2fbe7593810bc8577d8
    Merged-In: I84968fc0d6f51d2c38416403a1f2e0a9faf9589e
    Change-Id: I84968fc0d6f51d2c38416403a1f2e0a9faf9589e

--- services/core/java/com/android/server/pm/PackageManagerService.java
+++ services/core/java/com/android/server/pm/PackageManagerService.java
@@ -20041,7 +20041,8 @@ public class PackageManagerService extends IPackageManager.Stub
 
     @Override
     public void clearApplicationUserData(final String packageName,
-            final IPackageDataObserver observer, final int userId) {
+            final IPackageDataObserver observer, final int userId,
+            boolean restorePregrantedPermissions) {
         mContext.enforceCallingOrSelfPermission(
                 android.Manifest.permission.CLEAR_APP_USER_DATA, null);
 
@@ -20064,7 +20065,8 @@ public class PackageManagerService extends IPackageManager.Stub
                     try (PackageFreezer freezer = freezePackage(packageName,
                             "clearApplicationUserData")) {
                         synchronized (mInstallLock) {
-                            succeeded = clearApplicationUserDataLIF(packageName, userId);
+                            succeeded = clearApplicationUserDataLIF(packageName, userId,
+                                    restorePregrantedPermissions);
                         }
                         synchronized (mPackages) {
                             mInstantAppRegistry.deleteInstantApplicationMetadataLPw(
@@ -20097,7 +20099,8 @@ public class PackageManagerService extends IPackageManager.Stub
         });
     }
 
-    private boolean clearApplicationUserDataLIF(String packageName, int userId) {
+    private boolean clearApplicationUserDataLIF(String packageName, int userId,
+            boolean restorePregrantedPermissions) {
         if (packageName == null) {
             Slog.w(TAG, "Attempt to delete null packageName.");
             return false;
