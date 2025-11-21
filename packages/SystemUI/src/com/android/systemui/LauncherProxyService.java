a247e25e1af7588443e6b6124a029a0562920cc3
Michael Bestas <mkbestas@lineageos.org>
2025-11-21 15:33:37 +0200

    fixup! SystemUIProxy: Add injectLongPress()
    Change-Id: I8882a92300400c660f71715ee58ef3f6d53a1666

--- packages/SystemUI/src/com/android/systemui/LauncherProxyService.java
+++ packages/SystemUI/src/com/android/systemui/LauncherProxyService.java
@@ -362,13 +362,13 @@ public class LauncherProxyService implements CallbackController<LauncherProxyLis
 
         @Override
         public void injectLongPress(int keyCode) throws RemoteException {
-            final int displayId = keyEvent == null ? INVALID_DISPLAY : keyEvent.getDisplayId();
             verifyCallerAndClearCallingIdentityPostMain("longPressInjected", () -> {
-                sendEvent(KeyEvent.ACTION_DOWN, keyCode, 0, 0);
+                sendEvent(KeyEvent.ACTION_DOWN, keyCode, INVALID_DISPLAY, 0, 0);
                 mHandler.postDelayed(() -> {
-                    sendEvent(KeyEvent.ACTION_DOWN, keyCode, displayId, 1,
+                    sendEvent(KeyEvent.ACTION_DOWN, keyCode, INVALID_DISPLAY, 1,
                             KeyEvent.FLAG_LONG_PRESS);
-                    sendEvent(KeyEvent.ACTION_UP, keyCode, displayId, 0, KeyEvent.FLAG_CANCELED);
+                    sendEvent(KeyEvent.ACTION_UP, keyCode, INVALID_DISPLAY, 0,
+                            KeyEvent.FLAG_CANCELED);
                 }, ViewConfiguration.getLongPressTimeout());
             });
         }
