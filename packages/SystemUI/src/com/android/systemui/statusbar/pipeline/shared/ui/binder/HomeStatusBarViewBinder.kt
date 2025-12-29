7485ce8fa6105cfae9bf4c5bb2c663f6e8ddb76e
Bruno Martins <bgcngm@gmail.com>
2025-12-29 23:59:33 +0000

    SystemUI: Re-implement clock auto-hiding feature
    Co-authored-by: Cédric Bellegarde <cedric.bellegarde@adishatz.org>
    Change-Id: Idb0501b3d6903eac8e3768b25fd9b80852ba8fb7

--- packages/SystemUI/src/com/android/systemui/statusbar/pipeline/shared/ui/binder/HomeStatusBarViewBinder.kt
+++ packages/SystemUI/src/com/android/systemui/statusbar/pipeline/shared/ui/binder/HomeStatusBarViewBinder.kt
@@ -18,6 +18,7 @@ package com.android.systemui.statusbar.pipeline.shared.ui.binder
 
 import android.animation.Animator
 import android.animation.AnimatorListenerAdapter
+import android.app.WindowConfiguration
 import android.content.ContentResolver
 import android.database.ContentObserver
 import android.net.Uri
@@ -35,6 +36,9 @@ import com.android.systemui.display.dagger.SystemUIDisplaySubcomponent.PerDispla
 import com.android.systemui.lifecycle.repeatWhenAttached
 import com.android.systemui.res.R
 import com.android.systemui.scene.shared.flag.SceneContainerFlag
+import com.android.systemui.shared.system.ActivityManagerWrapper
+import com.android.systemui.shared.system.TaskStackChangeListener
+import com.android.systemui.shared.system.TaskStackChangeListeners
 import com.android.systemui.statusbar.chips.mediaprojection.domain.model.MediaProjectionStopDialogModel
 import com.android.systemui.statusbar.chips.ui.binder.OngoingActivityChipBinder
 import com.android.systemui.statusbar.chips.ui.binder.OngoingActivityChipViewBinding
@@ -99,6 +103,7 @@ constructor(
     }
 
     private data class ClockState(
+        val autoHide: Boolean,
         val denyListed: Boolean,
         val hideForHun: Boolean,
         val position: Int,
@@ -139,6 +144,7 @@ constructor(
                 val clockState =
                     MutableStateFlow(
                         ClockState(
+                            autoHide = false,
                             denyListed = false,
                             hideForHun = false,
                             position = context.contentResolver.readClockPosition(),
@@ -146,16 +152,50 @@ constructor(
                         )
                     )
 
+                val clockAutoHideUri: Uri =
+                    LineageSettings.System.getUriFor(
+                        LineageSettings.System.STATUS_BAR_CLOCK_AUTO_HIDE
+                    )
                 val iconHideListUri: Uri =
                     Settings.Secure.getUriFor(StatusBarIconController.ICON_HIDE_LIST)
                 val statusBarClockUri: Uri =
                     LineageSettings.System.getUriFor(LineageSettings.System.STATUS_BAR_CLOCK)
+
+                val taskStackListener =
+                    object : TaskStackChangeListener {
+                        override fun onTaskStackChanged() {
+                            val autoHide = computeClockAutoHide()
+                            clockState.update { state ->
+                                if (state.autoHide == autoHide) state
+                                else state.copy(autoHide = autoHide)
+                            }
+                        }
+                    }
 
                 val contentObserver =
                     object : ContentObserver(Handler(Looper.getMainLooper())) {
                         override fun onChange(selfChange: Boolean, uri: Uri?) {
                             clockState.update { current ->
                                 when (uri) {
+                                    clockAutoHideUri -> {
+                                        val enabled =
+                                            LineageSettings.System.getIntForUser(
+                                                context.contentResolver,
+                                                LineageSettings.System.STATUS_BAR_CLOCK_AUTO_HIDE,
+                                                0,
+                                                UserHandle.USER_CURRENT,
+                                            ) != 0
+
+                                        if (enabled) {
+                                            TaskStackChangeListeners.getInstance()
+                                                .registerTaskStackListener(taskStackListener)
+                                        } else {
+                                            TaskStackChangeListeners.getInstance()
+                                                .unregisterTaskStackListener(taskStackListener)
+                                        }
+
+                                        current.copy(autoHide = enabled && computeClockAutoHide())
+                                    }
                                     iconHideListUri ->
                                         current.copy(
                                             denyListed =
@@ -179,6 +219,12 @@ constructor(
                     }
 
                 context.contentResolver.registerContentObserver(
+                    clockAutoHideUri,
+                    false,
+                    contentObserver,
+                    UserHandle.USER_ALL,
+                )
+                context.contentResolver.registerContentObserver(
                     iconHideListUri,
                     false,
                     contentObserver,
@@ -191,6 +237,7 @@ constructor(
                     UserHandle.USER_ALL,
                 )
 
+                contentObserver.onChange(false, clockAutoHideUri)
                 contentObserver.onChange(false, iconHideListUri)
                 contentObserver.onChange(false, statusBarClockUri)
 
@@ -199,6 +246,8 @@ constructor(
                 job?.invokeOnCompletion {
                     runCatching {
                         context.contentResolver.unregisterContentObserver(contentObserver)
+                        TaskStackChangeListeners.getInstance()
+                            .unregisterTaskStackListener(taskStackListener)
                     }
                 }
 
@@ -407,6 +456,7 @@ constructor(
                                 if (
                                     state.visibilityModel.visibility == View.VISIBLE &&
                                         !hunBlocksClock &&
+                                        !state.autoHide &&
                                         !state.denyListed
                                 ) {
                                     state.visibilityModel
@@ -518,6 +568,14 @@ constructor(
             CLOCK_POSITION_LEFT,
             UserHandle.USER_CURRENT,
         )
+    }
+
+    private fun computeClockAutoHide(): Boolean {
+        return ActivityManagerWrapper.getInstance()
+            .runningTask
+            ?.configuration
+            ?.windowConfiguration
+            ?.activityType == WindowConfiguration.ACTIVITY_TYPE_HOME
     }
 
     private fun SystemEventAnimationState.isAnimatingChip() =
