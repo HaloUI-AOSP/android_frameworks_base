a7475dad77e8a79b863cf4b689fe8bb0d7be8b9a
Darshil Shah <darshilshah@google.com>
2026-05-29 10:33:07 +0000

    Validate caller UID in openProxyFileDescriptor
    Ensure that the calling UID matches the owner UID of the AppFuse mount
    before opening the file descriptor to prevent unauthorized cross-app access.
    
    Bug: 491748298
    Test: EXEMPT tested manually
    Flag: EXEMPT BUGFIX
    Cherrypick-From: https://googleplex-android-review.googlesource.com/q/commit:7231c8493ec30c2fab8d9e6de0fd842ffb5a37c8
    Merged-In: I83e8105de792c1a805cf029e7cbf57a611786995
    Change-Id: I83e8105de792c1a805cf029e7cbf57a611786995

--- services/core/java/com/android/server/StorageManagerService.java
+++ services/core/java/com/android/server/StorageManagerService.java
@@ -3704,6 +3704,7 @@ class StorageManagerService extends IStorageManager.Stub
     public @Nullable ParcelFileDescriptor openProxyFileDescriptor(
             int mountId, int fileId, int mode) {
         Slog.v(TAG, "mountProxyFileDescriptor");
+        final int uid = Binder.getCallingUid();
 
         // We only support a narrow set of incoming mode flags
         mode &= MODE_READ_WRITE;
@@ -3714,7 +3715,7 @@ class StorageManagerService extends IStorageManager.Stub
                     Slog.e(TAG, "FuseBridge has not been created");
                     return null;
                 }
-                return mAppFuseBridge.openFile(mountId, fileId, mode);
+                return mAppFuseBridge.openFile(uid, mountId, fileId, mode);
             }
         } catch (FuseUnavailableMountException | InterruptedException error) {
             Slog.v(TAG, "The mount point has already been invalid", error);
